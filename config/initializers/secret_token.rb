ca7d0680693fb467dbe0391d7dfb55bda2ae1234
Jevon Yeoh <jevonyeoh@gmail.com>
2013-01-05 18:43:39 -1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diaspura::Application.config.secret_token = 'b627b554b41db733a8fefddf351512fdc7fcfa572d6af9dbfd9d9150c545d5fd075158dffe0e3568612bc951b9c71de1fe6d82615832aae0ff39d89f923cd592'
